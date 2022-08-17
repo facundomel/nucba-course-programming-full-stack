d93762fb1c02a602e6bc5d46da8fccc572965633
facundomel <facundo.melgarr@gmail.com>
2022-08-17 00:24:32 -0300

    Finalización de challenge requerido 1 del modulo 2.

---
+++ module2_frontend_js/2_required/challenge1_array-of-objects_loops_methods/src/index.js
@@ -0,0 +1,49 @@
+import Pizza from "./model/Pizza.js";
+
+// Array de objetos de tipo Pizza
+const pizzas = [
+  new Pizza(1, "Pizza 1", ["Tomate"], 500),
+  new Pizza(2, "Pizza 2", ["Anchoas"], 1250),
+  new Pizza(3, "Pizza 3", ["Paleta", "Aceitunas"], 1000),
+  new Pizza(4, "Pizza 4", ["Queso roquefort"], 1100),
+  new Pizza(5, "Pizza 5", ["Cebolla"], 890),
+  new Pizza(6, "Pizza 6", ["Salame", "Aceitunas"], 900)
+]
+
+// Ejercicio 1
+function getPizzasIDOdd() {
+  console.log("Pizzas con ID impar: ");
+  pizzas.forEach((pizza) =>{
+    pizza.id % 2 != 0 ? console.log(`La pizza "${pizza.name}" tiene el ID ${pizza.id}`) : null;
+  });
+}
+
+// Ejercicio 2
+function getPizzasPriceLessThan600() {
+  console.log(pizzas.some((pizza) => {
+    return pizza.price < 600;
+  }) ? "Hay pizzas con precio menor que $600" : "No hay pizzas con precio menor que $600");
+}
+
+// Ejercicio 3
+function getNamePizzaAndPrice() {
+  pizzas.forEach((pizza) => {
+    console.log(`La pizza "${pizza.name}" tiene un valor de $${pizza.price}`);
+  });
+}
+
+// Ejercicio 4
+function getIngredientsPizza() {
+  pizzas.forEach((pizza) => {
+    console.log(`La pizza "${pizza.name}" tiene los siguientes ingredientes:`);
+    pizza.ingredients.forEach((ingredient) =>{
+      console.log(`- ${ingredient}`);
+    })
+  })
+}
+
+// Invocación de métodos
+getPizzasIDOdd();
+getPizzasPriceLessThan600();
+getNamePizzaAndPrice();
+getIngredientsPizza();
---
+++ module2_frontend_js/2_required/challenge1_array-of-objects_loops_methods/src/model/Pizza.js
@@ -0,0 +1,8 @@
+export default class Pizza {
+  constructor(id, name, ingredients, price) {
+    this.id = id;
+    this.name = name;
+    this.ingredients = ingredients;
+    this.price = price;
+  }
+}
