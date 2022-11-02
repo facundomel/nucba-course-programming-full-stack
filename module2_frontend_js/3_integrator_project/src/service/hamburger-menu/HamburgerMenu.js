a47922af3f6671f86ad655e5c2555046f376e81e
facundomel <facundo.melgarr@gmail.com>
2022-11-01 23:44:22 -0300

    Modified hamburger menu.

--- module2_frontend_js/3_integrator_project/src/service/hamburger-menu/HamburgerMenu.js
+++ module2_frontend_js/3_integrator_project/src/service/hamburger-menu/HamburgerMenu.js
@@ -1,12 +1,11 @@
-const containerHumburgerMenu = document.querySelector(".container-hamburger-menu");
-const btnHamburgerMenu = document.querySelector(".btn-hamburger-menu");
+const hamburgerMenuToggle = document.getElementById("hamburger-menu-toggle");
+const btnHamburgerMenu = document.querySelector(".hamburger-menu-button");
 const overlay = document.querySelector(".overlay");
 const btnCart = document.getElementById("btn-cart");
 const cart = document.getElementById("cart");
 
 export default new (class HamburgerMenu {
 	#toggleMenu() {
-		containerHumburgerMenu.classList.toggle("show-menu");
 		// if (cart.classList.contains("open-cart")) {
 		// 	cart.classList.remove("open-cart");
 		// 	return;
@@ -16,17 +15,17 @@ export default new (class HamburgerMenu {
 
 	#toggleCart() {
 		cart.classList.toggle("open-cart");
-		if (containerHumburgerMenu.classList.contains("show-menu")) {
-			containerHumburgerMenu.classList.remove("show-menu");
+		if (hamburgerMenuToggle.classList.contains("show-menu")) {
+			hamburgerMenuToggle.classList.remove("show-menu");
 			return;
 		}
 		overlay.classList.toggle("show-overlay");
 	}
 
 	#closeMenu() {
-		if (!containerHumburgerMenu.classList.contains("show-menu")) return;
-		containerHumburgerMenu.classList.remove("show-menu");
+		if (!hamburgerMenuToggle.checked) return;
 		overlay.classList.remove("show-overlay");
+		hamburgerMenuToggle.checked = false;
 	}
 
 	#scroll() {
