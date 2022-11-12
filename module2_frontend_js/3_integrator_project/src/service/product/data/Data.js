32ec4abfb206fc31dc3a0e28088a5ca29e500a68
facundomel <facundo.melgarr@gmail.com>
2022-11-12 01:06:42 -0300

    Added products images.

--- module2_frontend_js/3_integrator_project/src/service/product/data/Data.js
+++ module2_frontend_js/3_integrator_project/src/service/product/data/Data.js
@@ -14,6 +14,7 @@ export default new (class Data {
 				true,
 				"65.755,00"
 			),
+
 			new Product(
 				2,
 				"Xbox Series S",
@@ -24,6 +25,7 @@ export default new (class Data {
 				true,
 				"139.999,00"
 			),
+
 			new Product(
 				3,
 				"Nintendo Wii",
@@ -33,32 +35,121 @@ export default new (class Data {
 				"consoles",
 				false
 			),
+
 			new Product(
 				4,
 				"PlayStation 4",
 				"Sony PlayStation 4 500GB Standard color negro azabache",
 				"135.000,00",
 				"./assets/products/consoles/play-station-4.png",
+				"consoles",
+				false
+			),
+
+			// Notebooks
+			new Product(
+				5,
+				"Notebook Asus ZenBook 14 UM425QA",
+				'Notebook Asus ZenBook 14 UM425QA pine gray 14", AMD Ryzen 5 4500U 8GB de RAM 512GB SSD, AMD Radeon RX Vega 6 (Ryzen 4000/5000) 1920x1080px Windows 11 Home',
+				"269.999,00",
+				"./assets/products/notebooks/asus-zenbook.png",
 				"notebooks",
 				false
 			),
 
-			// new Product(5, "Leo Messi", "¡De pié señores!", 350, "./assets/img/pizza/leomessi.png", "Pizza", false, false, 1),
-			// new Product(6, "Nick Gi", "La que desaparece", 350, "./assets/img/pizza/nickgi.png", "Pizza", false, false, 1),
-			// new Product(7, "Picantovich", "Pica 2 veces", 350, "./assets/img/pizza/picantovich.png", "Pizza", false, false, 1),
-			// new Product(8, "¡Q' Jamone!", "c/jamón crudo", 350, "./assets/img/pizza/qjamone.png", "Pizza", false, false, 1),
+			new Product(
+				6,
+				"Notebook Dell Inspiron 3515",
+				'Notebook Dell Inspiron 3515 plateada 15.5", AMD Ryzen 5 3450U 8GB de RAM 256GB SSD, AMD Radeon RX Vega 8 (Ryzen 2000/3000) 1366x768px Windows 11 Home',
+				"144.399,00",
+				"./assets/products/notebooks/dell-inspiron.png",
+				"notebooks",
+				true,
+				"159.999,00"
+			),
 
-			// new Product(9, "Clásica", "Para conservadores", 350, "./assets/img/hamburguesa/hclasica.jpg", "Hamburguesa", false, false, 1),
-			// new Product(10, "Monstruosa", "No le tengas miedo", 350, "./assets/img/hamburguesa/hmonstruosa.jpg", "Hamburguesa", true, true, 1),
-			// new Product(11, "Vegeta", "Voy a hacerte pedazos", 350, "./assets/img/hamburguesa/hveggie.jpg", "Hamburguesa", true, false, 1),
+			new Product(
+				7,
+				"Notebook Lenovo IdeaPad 14IIL05 platinum gray",
+				'Notebook Lenovo IdeaPad 14IIL05 platinum gray 14", Intel Core i5 1035G1 8GB de RAM 512GB SSD, Intel UHD Graphics G1 1920x1080px Windows 10 Home',
+				"185.147,00",
+				"./assets/products/notebooks/lenovo-ideapad.png",
+				"notebooks",
+				false
+			),
 
-			// new Product(12, "Cheddar y Panceta", "Experiencia única", 350, "./assets/img/papasFritas/cheddarpanceta.jpg", "Papas fritas", true, false, 1),
-			// new Product(13, "Clásicas", "Cortita y al pié", 350, "./assets/img/papasFritas/clasicas.jpg", "Papas fritas", false, false, 1),
-			// new Product(14, "Provensal", "¿Viniste sólo no?", 350, "./assets/img/papasFritas/provensal.jpg", "Papas fritas", true, false, 1),
+			// Celulares
+			new Product(
+				8,
+				"Moto G22",
+				"Moto G22 128 GB iceberg blue 4 GB RAM",
+				"51.999,00",
+				"./assets/products/cellphones/moto-g22.png",
+				"cellphones",
+				false
+			),
 
-			// new Product(15, "Carnitas", "Mooooo", 350, "./assets/img/wrap/wcarnitas.jpg", "Wrap", true, true, 1),
-			// new Product(16, "Pollito", "Pio pio", 350, "./assets/img/wrap/wpollito.jpg", "Wrap", true, false, 1),
-			// new Product(17, "veggie", "sonido vegetal", 350, "./assets/img/wrap/wveggie.jpg", "Wrap", false, false, 1),
+			new Product(
+				9,
+				"Samsung Galaxy A03",
+				"Samsung Galaxy A03 128 GB negro 4 GB RAM",
+				"49.999,00",
+				"./assets/products/cellphones/samsung-galaxy-a03.png",
+				"cellphones",
+				false
+			),
+
+			new Product(
+				10,
+				"Xiaomi Redmi Note 11",
+				"Xiaomi Redmi Note 11 (Snapdragon) Dual SIM 128 GB gris grafito 4 GB RAM",
+				"79.999,00",
+				"./assets/products/cellphones/xiaomi-redmi-note-11.png",
+				"cellphones",
+				false
+			),
+
+			new Product(
+				11,
+				"Apple iPhone 11",
+				"Apple iPhone 11 (128 GB) - Negro",
+				"231.084,00",
+				"./assets/products/cellphones/iphone-11.png",
+				"cellphones",
+				false
+			),
+
+			// Smart watch
+			new Product(
+				14,
+				"Smartwatch Noga NG-SW05",
+				'Smartwatch Noga NG-SW05 1.3" caja de metal plateada, malla negra de tpu',
+				"10.250,00",
+				"./assets/products/smart_watch/noga_ng_sw05.png",
+				"smart-watch",
+				true,
+				"11.780,00"
+			),
+
+			new Product(
+				15,
+				"Xiaomi Redmi Watch 2 Lite",
+				'Xiaomi Redmi Watch 2 Lite 1.55" caja black, malla black de tpu',
+				"17.799,00",
+				"./assets/products/smart_watch/xiaomi_redmi_watch_2_lite.png",
+				"smart-watch",
+				false,
+			),
+
+			new Product(
+				16,
+				"Samsung Galaxy Watch4",
+				'Samsung Galaxy Watch4 (Bluetooth) 1.4" caja 44mm de aluminio black, malla black de fluoroelastómero SM-R870',
+				"55.990,00",
+				"./assets/products/smart_watch/samsung_galaxy_watch4.png",
+				"smart-watch",
+				false
+			),
 		];
 	}
 })();
