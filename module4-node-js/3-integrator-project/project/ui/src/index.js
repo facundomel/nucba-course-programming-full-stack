3e74a0dbbd7653a4f8d6afc3fa41dd9e458dbb8f
facundomel <facundo.melgarr@gmail.com>
2023-08-15 00:54:35 -0300

    Added line for test environment variable in integrator project of module 4.

--- module4-node-js/3-integrator-project/project/ui/src/index.js
+++ module4-node-js/3-integrator-project/project/ui/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
